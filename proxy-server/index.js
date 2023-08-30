68db717fb3e84e07ecdf0dc976c2ba066f7b5239
Conor <conor.tighe22@gmail.com>
2023-08-30 15:57:47 +0100

    update server

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -6,13 +6,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 const app = express()
 
 dotenv.config();
-console.log(process.env.PEXEL_API_KEY);
 
 app.use(cors())
 
+console.log(process.env.PEXEL_API_KEY)
+
 app.use(function(req, res, next) {
-    console.log("test");
-    console.log(process.env.PEXEL_API_KEY);
     res.setHeader('Authorization', process.env.PEXEL_API_KEY)
     next()
 });
